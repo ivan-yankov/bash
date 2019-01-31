b67ab952d706907be5730d45ff3ec7ab2b071062
yankov <ivan.iankov@gmail.com>
2019-01-31 22:36:00 +0200

    Improve Derby installation

--- export-path.sh
+++ export-path.sh
@@ -7,7 +7,8 @@ export PATH=$PATH:$JRE_HOME
 export M2_HOME=/opt/maven/bin
 export PATH=$PATH:$M2_HOME
 
-export DERBY_INSTALL=/opt/db-derby
+export DERBY_INSTALL=/opt/db-derby/bin
+export PATH=$PATH:$DERBY_INSTALL
 
 export DOTNET_ROOT=/opt/dotnet 
 export PATH=$PATH:$DOTNET_ROOT
