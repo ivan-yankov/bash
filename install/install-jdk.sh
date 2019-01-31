5657eb556677175c468c5af5ad77b17da4c59c6d
yankov <ivan.iankov@gmail.com>
2019-01-31 23:43:58 +0200

    Improve JDK installation

--- install/install-jdk.sh
+++ install/install-jdk.sh
@@ -10,9 +10,6 @@ function install-jdk {
     
     sudo tar -xvzf $jdk_file --directory $dest_dir
     
-    sudo update-alternatives --install /usr/bin/java java $dest_dir/$jdk/jre/bin/java 1
-    sudo update-alternatives --config java
-    
     sudo chown --recursive yankov $dest_dir/$jdk
     
     echo "Installation successful"
