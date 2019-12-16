130409e915fff666e7ef589e17a7bc409ff67f6b
yankov <ivan.iankov@gmail.com>
2019-12-16 21:54:38 +0200

    Improve exporting path

--- export-path.sh
+++ export-path.sh
@@ -1,17 +1,7 @@
 export BASH_SCRIPTS=~/data/repos/bash
 export PATH=$PATH:$BASH_SCRIPTS
 
-export JAVA_HOME=/opt/jdk1.8.0_161
-export PATH=$PATH:$JAVA_HOME
-
-export JRE_HOME=$JAVA_HOME/jre
-export PATH=$PATH:$JRE_HOME
-
-export M2_HOME=/opt/maven/bin
-export PATH=$PATH:$M2_HOME
-
-export DERBY_INSTALL=/opt/db-derby/bin
-export PATH=$PATH:$DERBY_INSTALL
-
-export DOTNET_ROOT=/opt/dotnet 
-export PATH=$PATH:$DOTNET_ROOT
+file=$BASH_SCRIPTS/custom-export-path.sh
+if [ -f $file ]; then
+   source $file
+fi
