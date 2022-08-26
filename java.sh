b88914149981fcb6ef4849dcd4f107433a7b1523
ivan-yankov <ivan.iankov@gmail.com>
2022-08-26 12:03:20 +0300

    Refactoring, java download and install

---
+++ java.sh
@@ -0,0 +1,28 @@
+function java-download {
+  image_type=$1 # jdk, jre
+  version=$2 # 1, 2, 3, ...
+
+  curl -X GET -L https://api.adoptium.net/v3/binary/latest/$version/ga/linux/x64/$image_type/hotspot/normal/eclipse --output $image_type-$version.tar.gz
+}
+
+function java-install {
+  image_type=$1 # jdk, jre
+  version=$2 # 1, 2, 3, ...
+  archive_file=$image_type-$version.tar.gz
+
+  untargz $archive_file
+  d=$(lstargz $archive_file | head -1 | cut -f1 -d "/")
+
+  sudo mkdir -p /opt/java
+  dest=/opt/java/$image_type-$version
+  sudo rm -rf $dest
+  sudo mv $d $dest
+}
+
+function java-set {
+  image_type=$1 # jdk, jre
+  version=$2 # 1, 2, 3, ...
+
+  sudo rm /usr/bin/java
+  sudo ln -s /opt/java/$image_type-$version/bin/java /usr/bin/java
+}
--- media.sh
+++ media.sh
@@ -1,4 +1,29 @@
+YOUTUBE_DOWNLOADER_DIR="/opt/youtube-downloader"
+YOUTUBE_DOWNLOADER_FILE="youtube-dl"
+
 function flac-test {
   dir=$1
   find $dir -type f -iname '*.flac' -print0 | xargs --null flac -wst
+}
+
+function ytd-get {
+  sudo mkdir -p $YOUTUBE_DOWNLOADER_DIR
+  sudo curl -X GET -L https://yt-dl.org/downloads/latest/youtube-dl --output $YOUTUBE_DOWNLOADER_DIR/$YOUTUBE_DOWNLOADER_FILE
+}
+
+function ytd-remove {
+  sudo rm $YOUTUBE_DOWNLOADER_DIR/$YOUTUBE_DOWNLOADER_FILE
+}
+
+function ytd-help {
+  python $YOUTUBE_DOWNLOADER_DIR/$YOUTUBE_DOWNLOADER_FILE --help
+}
+
+function ytd-formats {
+  python $YOUTUBE_DOWNLOADER_DIR/$YOUTUBE_DOWNLOADER_FILE -F $1
+}
+
+function ytd-download {
+  # provide format and url
+  python $YOUTUBE_DOWNLOADER_DIR/$YOUTUBE_DOWNLOADER_FILE -f "$@"
 }
