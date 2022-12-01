e7c390d6558f3239ed090a089eb9c1cd72996790
Ivan <ivan.iankov@gmail.com>
2022-12-01 17:35:09 +0200

    Fixes

--- java.sh
+++ java.sh
@@ -1,42 +1,44 @@
 function java-download {
   is-defined $1 && is-defined $2 && is-defined $3 || return 1
 
-  image_type=$1 # jdk, jre
-  version=$2 # 1, 2, 3, ...
-  dest=$3
+  local image_type=$1 # jdk, jre
+  local version=$2 # 1, 2, 3, ...
+  local dest=$3
 
   curl -X GET -L https://api.adoptium.net/v3/binary/latest/$version/ga/linux/x64/$image_type/hotspot/normal/eclipse --output $dest/$image_type-$version.tar.gz
 }
 
 function java-extract {
   is-defined $1 || return 1
-  archive=$1
+  local archive=$1
 
   untargz $archive
-  d=$(targz-root $archive)
+  local d=$(targz-root $archive)
 
   mv $d $(dirname $archive)/$(basename $archive .tar.gz)
 }
 
 function java-install {
-  is-defined $1 && is-defined $2 && is-defined $3 || return 1
+  is-defined $1 && is-defined $2 || return 1
 
-  image_type=$1 # jdk, jre
-  version=$2 # 1, 2, 3, ...
+  local image_type=$1 # jdk, jre
+  local version=$2 # 1, 2, 3, ...
 
-  java-download $image_type $version /tmp
-  java-extract /tmp/$image_type-$version.tar.gz
+  local archive=/tmp/$image_type-$version.tar.gz
 
-  dest=/opt/java/$image_type-$version
+  java-download $image_type $version $(dirname $archive)
+  java-extract $archive
+
+  local dest=/opt/java/$image_type-$version
   sudo mkdir -p $(dirname $dest)
   sudo rm -rf $dest
-  sudo mv $d $dest
+  sudo mv $(dirname $archive)/$image_type-$version $dest
   rm $archive
 }
 
 function java-set {
-  image_type=$1 # jdk, jre
-  version=$2 # 1, 2, 3, ...
+  local image_type=$1 # jdk, jre
+  local version=$2 # 1, 2, 3, ...
 
   sudo rm /usr/bin/java
   sudo ln -s /opt/java/$image_type-$version/bin/java /usr/bin/java
