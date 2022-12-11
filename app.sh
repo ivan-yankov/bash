1fd5a6b4d4216c5a58914ead6306175228f0f228
Ivan <ivan.iankov@gmail.com>
2022-12-11 18:49:29 +0200

    Improvements

--- app.sh
+++ app.sh
@@ -2,20 +2,27 @@ function release-app-image {
   local builder_dir=$REPOS/app-image-builder
   local project_dir=$PWD
   local cache_dir=~/.app-image-builder/cache
-  local ini_file=$project_dir/release.ini
-  local application_name=$(get-ini-value ApplicationName $ini_file)
-  local app_image=$APP_IMAGE/$application_name.AppImage
+
+  source $builder_dir/build-jvm-based-app-image.sh
 
   sudo mkdir -p $APP_IMAGE
 
-  $project_dir/build.sh
+  $project_dir/build.sh && \
 
-  source $builder_dir/build-jvm-based-app-image.sh
-  build-jvm-based-app-image $ini_file $builder_dir/resources $cache_dir $app_image
+  for ini_file in $project_dir/*.ini; do
+    local application_name=$(get-ini-value ApplicationName $ini_file)
+    local app_image=$APP_IMAGE/$application_name.AppImage
+
+    build-jvm-based-app-image $ini_file $builder_dir/resources $cache_dir $app_image && \
+    app-image-alias $application_name
+  done
 }
 
 function app-image-alias {
-  local ini_file=$PWD/release.ini
-  local application_name=$(get-ini-value ApplicationName $ini_file)
-  echo "alias $application_name=\"$APP_IMAGE/$application_name.AppImage $@\"" > "$BASH_LOCAL/$application_name.sh"
+  is-defined $1 || return 1
+  local application_name=$1
+  local out=$BASH_LOCAL/$application_name.sh
+  if [ ! -f $out ]; then
+    echo "alias $application_name=\"$APP_IMAGE/$application_name.AppImage \"" > $out
+  fi
 }
