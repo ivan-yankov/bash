859230e504c0c956cefb17b58cb4fc34bf659e45
Ivan <ivan.iankov@gmail.com>
2023-01-08 17:29:00 +0200

    Improvement

--- install.sh
+++ install.sh
@@ -47,17 +47,27 @@ function install-openjfx {
 function install-micro {
   is-defined $PROGRAMS || return 1
   local dir=$PROGRAMS/micro
+
   sudo apt install xclip
   sudo mkdir -p $dir
   sudo curl -sL https://gist.githubusercontent.com/zyedidia/d4acfcc6acf2d0d75e79004fa5feaf24/raw/a43e603e62205e1074775d756ef98c3fc77f6f8d/install_micro.sh | sudo bash -s linux64 $dir
+
   sudo rm /usr/bin/editor
   sudo ln -s $dir/micro /usr/bin/editor
+
+  sudo rm /usr/bin/view
+  sudo ln -s $dir/micro /usr/bin/view
 }
 
 function uninstall-micro {
   is-defined $PROGRAMS || return 1
+
   sudo rm /usr/bin/editor
   sudo ln -s /bin/nano /usr/bin/editor
+
+  sudo rm /usr/bin/view
+  sudo ln -s /bin/nano /usr/bin/view
+
   sudo rm -rf $PROGRAMS/micro
 }
 
