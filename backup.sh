1574c39cbafdf08bdd27392ccafd80355a8bf93a
Ivan <ivan.iankov@gmail.com>
2023-02-02 20:31:33 +0200

    Fix

--- backup.sh
+++ backup.sh
@@ -16,9 +16,9 @@ function backup-sync {
 
 function backup-sync-parallel {
   is-defined $1 && is-defined $2 && is-defined $3 || return 1
-  local src=$1
-  local dest=$2
-  local number_of_processes=$3
+  local number_of_processes=$1
+  local src=$2
+  local dest=$3
   local exclude=$4
-  ls $src | xargs -n1 -P$number_of_processes -I% /bin/bash -c "source $BASH_SOURCE && backup-sync $exclude % $dest"
+  ls $src | xargs -n1 -P$number_of_processes -I% sudo rsync --delete --archive --checksum --copy-links $exclude $src $dest
 }
