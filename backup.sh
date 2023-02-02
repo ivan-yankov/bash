bd23df7e4c93bb69a8964776ee02bc4c68722be5
Ivan <ivan.iankov@gmail.com>
2023-02-02 16:02:13 +0200

    Parallel backup

--- backup.sh
+++ backup.sh
@@ -13,3 +13,11 @@ function backup-sync-quick {
 function backup-sync {
   sudo rsync --delete --archive --checksum --copy-links "$@"
 }
+
+function backup-sync-parallel {
+  is-defined $1 && is-defined $2 && is-defined $3 || return 1
+  local src=$1
+  local dest=$2
+  local number_of_processes=$3
+  ls $src | xargs -n1 -P$number_of_processes -I% /bin/bash -c "source $BASH_SOURCE && backup-sync % $dest"
+}
