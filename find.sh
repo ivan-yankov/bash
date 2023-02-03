a5b6d184d68dd10c8e26d5a436e9cc8b319f8de2
Ivan <ivan.iankov@gmail.com>
2023-02-03 15:29:30 +0200

    Improvement

--- find.sh
+++ find.sh
@@ -2,12 +2,12 @@ function find-file {
   is-defined $1 && is-defined $2 || return 1
   local dir="$1"
   local what="$2"
-  find $dir -name $what
+  sudo find $dir -name $what
 }
 
 function find-dir {
   is-defined $1 && is-defined $2 || return 1
   local dir="$1"
   local what="$2"
-  find $dir -type d -name $what
+  sudo find $dir -type d -name $what
 }
