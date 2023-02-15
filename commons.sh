7e7f3f92f0761c36b42c0251fa05c68d8ab5aca3
Ivan <ivan.iankov@gmail.com>
2023-02-15 14:15:25 +0200

    Improvements

--- commons.sh
+++ commons.sh
@@ -17,3 +17,8 @@ function grant-access-external-drive {
   is-defined $1 || return 1
   sudo chmod -R ugo+rwx $1
 }
+
+function title {
+  is-defined $1 || return 1
+  echo -ne "\033]30;$1\007"
+}
--- init.sh
+++ init.sh
@@ -7,10 +7,35 @@ function is-defined {
   fi
 }
 
+function collect-functions {
+  is-defined $1 || return 1
+  local file=$1
+  
+  result=()
+  items=`cat $file`
+  flag=0
+  for item in $items; do
+    if [ $flag == 1 ]; then
+      result+=("$item")
+      flag=0
+    fi
+    if [ $item == "function" ]; then
+      flag=1
+    fi
+  done
+
+  IFS=$'\n'
+  sorted=($(sort <<< "${result[*]}"))
+  unset IFS
+
+  for item in ${sorted[@]}; do
+    CMDS=$CMDS$'\n'$item
+  done
+}
+
 function load {
   is-defined $1 || return 1
-
-  dir=$1
+  local dir=$1
 
   for file in $dir/*; do
     if [ -f $file ]; then
@@ -20,13 +45,29 @@ function load {
       fi
       if [ ${file: -3} == ".sh" ]; then
         source $file
+        collect-functions $file
       fi
+    fi
+    if [ -d $file ]; then
+      load $file
     fi
   done
 }
 
+function cmds {
+  is-defined $CMDS || return 1
+  echo "$CMDS"
+}
+
+function manual-installed-packages {
+  # manually installed packages without dependencies
+  comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)
+}
+
 function init {
   export BASH_LOCAL=~/.bash
+  export CMDS=()
+
   load $BASH_LOCAL
   load $(dirname $BASH_SOURCE)
 }
