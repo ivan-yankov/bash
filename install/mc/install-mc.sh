5f7b2940a84592a7c57ba44baac7f509b8190525
Ivan Yankov <ivan.iankov@gmail.com>
2023-02-24 08:36:07 +0200

    Improvement

--- install/mc/install-mc.sh
+++ install/mc/install-mc.sh
@@ -5,10 +5,6 @@ function install-mc {
   local cfg=~/.config/mc
 
   mkdir -p $cfg
-  if [ -f $cfg/mc.ext  ]; then
-    mv $cfg/mc.ext $cfg/mc.ext.original
-  fi
-
   local src=$(dirname $BASH_SOURCE)
   cp $src/ini $cfg
   cp $src/mc.ext $cfg
