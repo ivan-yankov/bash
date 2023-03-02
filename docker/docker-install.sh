183f3c0c963f8636da8de6e72a8211d8cbf3be8a
Ivan Yankov <ivan.yankov@estafet.com>
2023-03-02 14:24:02 +0200

    Improvement

--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -1,32 +1,21 @@
-# dsc:Download and install docker.
+# dsc:Install docker.
 function docker-install {
-  local dest=/tmp
+  sudo apt-get update
+  sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-  local release=$(lsb_release -cs)
-  local arch=$(dpkg --print-architecture)
-  local url="https://download.docker.com/linux/ubuntu/dists/$release/pool/stable/amd64"
+  sudo mkdir -m 0755 -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-  local container_io_version="1.6.9-1"
-  local docker_ce_version="23.0.1-1~ubuntu.22.04~jammy"
-  local docker_ce_cli_version="23.0.1-1~ubuntu.22.04~jammy"
-  local docker_buildx_plugin_version="0.10.2-1~ubuntu.22.04~jammy"
-  local docker_compose_plugin_version="2.16.0-1~ubuntu.22.04~jammy"
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-  local f1=containerd.io_${container_io_version}_${arch}.deb
-  local f2=docker-ce_${docker_ce_version}_${arch}.deb
-  local f3=docker-ce-cli_${docker_ce_cli_version}_${arch}.deb
-  local f4=docker-buildx-plugin_${docker_buildx_plugin_version}_${arch}.deb
-  local f5=docker-compose-plugin_${docker_compose_plugin_version}_${arch}.deb
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  sudo apt-get update
 
-  curl -L $url/$f1 -o $dest/$f1
-  curl -L $url/$f2 -o $dest/$f2
-  curl -L $url/$f3 -o $dest/$f3
-  curl -L $url/$f4 -o $dest/$f4
-  curl -L $url/$f5 -o $dest/$f5
-
-  sudo dpkg -i $dest/$f1 $dest/$f2 $dest/$f3 $dest/$f4 $dest/$f5
-
-  rm $f1 $f2 $f3 $f4 $f5
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
   sudo groupadd docker
   sudo usermod -aG docker $USER
