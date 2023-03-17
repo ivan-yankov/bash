5565e14871efae35595f26c5de2360b58ef365d2
Ivan Yankov <ivan.iankov@gmail.com>
2023-03-17 15:40:33 +0200

    Improvement

--- app-image/app-image-build.sh
+++ app-image/app-image-build.sh
@@ -1,15 +1,16 @@
-# dsc:Build .AppImage file from the project directory.
-# dsc:Expects .ini file(s) to be defined with build details.
-# env:$PROGRAMS path to the programs directory
+# dsc:Build AppImage.
+# dsc:Expects build.sh script in project directory. It is used to build the project code base.
+# dsc:Expects ini file(s) to be defined with build details.
 # arg:$1 project directory
+# arg:$2 AppImage destination directory
 function app-image-release {
-  is-defined $PROGRAMS && is-defined $1 || return 1
+  is-defined $1 && is-defined $2 || return 1
 
   local project_dir=$1
+  local app_image_dir=$2
   local cache_dir=~/.app-image-builder/cache
-  local app_image_dir=$PROGRAMS/app-image
 
-  source $(dirname $(dirname $BASH_SOURCE))/app-image-builder/build-jvm-based-app-image.sh
+  source $(dirname $BASH_SOURCE)/../../app-image-builder/build-jvm-based-app-image.sh
 
   sudo mkdir -p $app_image_dir
 
