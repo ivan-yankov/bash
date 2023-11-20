f42700dc0c2714c5bfbb5f89223329f07e34f2c6
Ivan <ivan.iankov@gmail.com>
2023-11-20 10:57:43 +0200

    Update

--- find/find-any.sh
+++ find/find-any.sh
@@ -1,9 +1,8 @@
 # dsc:Find regular file, directory or symbolic link.
-# arg:$1 directory to search in
-# arg:$2 file name or wildcard to search for
+# arg:$1 file name or wildcard to search for
 function find-any {
-  is-defined $1 && is-defined $2 || return 1
-  local where="$1"
-  local what="$2"
-  sudo find $where -type f,d,l -name $what
+  is-defined $1 || return 1
+  local what="$1"
+  local where="."
+  find $where -type f,d,l -name $what 2>/dev/null
 }
--- find/find-link.sh
+++ find/find-link.sh
@@ -1,9 +1,8 @@
 # dsc:Find symbolic link.
-# arg:$1 directory to search in
-# arg:$2 file name or wildcard to search for
+# arg:$1 file name or wildcard to search for
 function find-link {
   is-defined $1 && is-defined $2 || return 1
-  local where="$1"
-  local what="$2"
-  sudo find $where -type l -name $what
+  local what="$1"
+  local where="."
+  find $where -type l -name $what 2>/dev/null
 }
