2b8d57b170d5afabc52a840c8a72767448b040e4
Ivan <ivan.iankov@gmail.com>
2024-08-04 16:20:10 +0300

    New shell

--- shell/newsh.sh
+++ shell/newsh.sh
@@ -1,15 +1,41 @@
-# dsc:Create new shell file function with documentation header.
-# arg:$1 function name
-function newsh {
-  is-defined $1 || return 1
-  local fn=$1
+function help-newsh {
+  echo "Create new shell command."
+  echo
+  echo "Usage: newsh cmd"
+}
 
-  local file=$fn.sh
+function newsh {
+  if [  $# -eq 0  ]; then
+    help-newsh
+    return 1
+  fi
+
+  if [[  $1 == "-h"  ]]; then
+    help-newsh
+    return 0
+  fi
+
+  local cmd=$1
+
+  local file=$cmd.sh
   touch $file
-  
-  echo '# dsc:<Command description>' >> $file
-  echo '# env:<Required environment variables>' >> $file
-  echo '# arg:$1 <Argument>' >> $file
-  echo "function $fn {" >> $file
+
+  echo "function help-$cmd {" >> $file
+  echo '  echo "DESCRIPTION."' >> $file
+  echo '  echo' >> $file
+  echo "  echo \"Usage: $cmd\"" >> $file
+  echo '}' >> $file
+  echo >> $file
+  echo "function $cmd {" >> $file
+  echo '  if [  $# -eq 0  ]; then' >> $file
+  echo "    help-$cmd" >> $file
+  echo '    return 1' >> $file
+  echo '  fi' >> $file
+  echo >> $file
+  echo '  if [[  $1 == "-h"  ]]; then' >> $file
+  echo "    help-$cmd" >> $file
+  echo '    return 0' >> $file
+  echo '  fi' >> $file
+  echo >> $file
   echo '}' >> $file
 }
