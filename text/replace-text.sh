49f0998b01929b4e737c751494f529bdb12ba9b8
Ivan <ivan.iankov@gmail.com>
2024-08-20 12:49:28 +0300

    Replace text

--- text/replace-text.sh
+++ text/replace-text.sh
@@ -1,12 +1,37 @@
-# dsc:Replace text in the specified file.
-# dsc:Text match is case sensitive.
-# arg:$1 text to search for
-# arg:$2 text to replace with
-# arg:$3 file to search in
+function help-replace-text {
+  echo "Replace text in the specified file."
+  echo "Creates backup for the file specified."
+  echo "Text match is case sensitive."
+  echo "Requires python."
+  echo
+  echo "Usage: replace-text what with file"
+  echo "  what: text to search for"
+  echo "  with: text to replace with"
+  echo "  file: file to search in"
+}
+
 function replace-text {
-  is-defined $1 && is-defined $2 && is-defined $3 || return 1
+  if [  $# -eq 0  ]; then
+    help-replace-text
+    return 1
+  fi
+
+  if [[  $1 == "-h"  ]]; then
+    help-replace-text
+    return 0
+  fi
+
   local what=$1
   local with=$2
-  local file=$3
-  sed -i "s/$what/$with/" $file
+  local file_name=$3
+
+  cp $file_name $file_name.bak
+
+  python -c "import sys
+with open(\"$file_name\", 'r') as file:
+  data = file.read()
+  data = data.replace(\"$what\", \"$with\")
+
+with open(\"$file_name\", 'w') as file:
+  file.write(data)"
 }
