d1ad312b41616bd0dda33133fd55ee950c3d2d51
Ivan <ivan.iankov@gmail.com>
2024-08-20 15:33:30 +0300

    Init improvement

--- cmds.sh
+++ cmds.sh
@@ -1,6 +1,15 @@
-# dsc:Show list of custom functions.
-# env:$BASH_LOCAL directory which contains bash files specific to the local machine
+function help-cmds {
+  echo "Show list of custom functions."
+  echo "Expects BASH_LOCAL environment variable."
+  echo "  BASH_LOCAL: directory which contains bash files specific to the local machine."
+}
+
 function cmds {
+  if [[  $1 == "-h"  ]]; then
+    help-cmds
+    return 0
+  fi
+
   collect-cmds $(dirname $BASH_SOURCE)
   collect-cmds $BASH_LOCAL
 }
--- init.sh
+++ init.sh
@@ -1,12 +1,28 @@
-# dsc:Load bash sources from the current directory and ~/.bash directory recursively.
-# dsc:This function should be defined as entry point in .bashrc.
+function help-init {
+  echo "Load bash sources from the bash project directory and ~/.bash directory recursively."
+  echo "This function should be defined as entry point in .bashrc."
+}
+
 function init {
+  if [[  $1 == "-h"  ]]; then
+    help-init
+    return 0
+  fi
+
   local root=$(dirname $BASH_SOURCE)
+  export BASH_LOCAL=~/.bash
+
   source $root/shell/is-defined.sh
   source $root/shell/load.sh
 
-  load $root
+  if test -f $BASH_LOCAL/env.sh; then
+    source $BASH_LOCAL/env.sh
+  fi
 
-  export BASH_LOCAL=~/.bash
+  if test -f $BASH_LOCAL/alias.sh; then
+    source $BASH_LOCAL/alias.sh
+  fi
+
+  load $root
   load $BASH_LOCAL
 }
