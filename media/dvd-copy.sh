80759eb141fdd9936975f2c39d59b4ba88416f21
Ivan <ivan.iankov@gmail.com>
2024-09-29 21:32:12 +0300

    DVD copy update

--- media/dvd-copy.sh
+++ media/dvd-copy.sh
@@ -1,11 +1,28 @@
-# dsc:Copy DVD disc files to specified directory.
-# dsc:Expects cdrom drive to be mount at /mnt/cdrom.
-# arg:$1 destination directory
+function help-dvd-copy {
+  echo "Copy DVD disc files to a specified subdirectory of the current one."
+  echo "Mounts optical drive to /mnt/cdrom."
+  echo
+  echo "Usage: dvd-copy dest-dir"
+}
+
 function dvd-copy {
-  is-defined $1 || return 1
+  if [  $# -eq 0  ]; then
+    help-dvd-copy
+    return 1
+  fi
+
+  if [[  $1 == "-h"  ]]; then
+    help-dvd-copy
+    return 0
+  fi
+
+  mnt-cdrom
+
   local src=/mnt/cdrom
   local dest=$1
   mkdir -p $dest
   sudo cp -r $src/VIDEO_TS $dest
   sudo chmod +rwx $dest/VIDEO_TS/
+
+  umnt-cdrom
 }
