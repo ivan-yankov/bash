2ee573ace9d5c05db1e5f03012328b4a56457059
ivan.yankov <ivan.yankov@gmail.com>
2025-02-16 10:11:53 +0200

    Changes

--- text/replace-all-text.sh
+++ text/replace-all-text.sh
@@ -1,8 +1,20 @@
-# dsc:Replace text in all files in the current directory.
-# dsc:Text match is case sensitive.
-# arg:$1 text to search for
-# arg:$2 text to replace with
+function help-replace-all-text {
+  echo "Replace text in all files in the current directory."
+  echo "Text match is case sensitive."
+  echo
+  echo "Usage: replace-all-text replace-what replace-width"
+}
+
 function replace-all-text {
-  is-defined $1 && is-defined $2 || return 1
+  if [  $# -eq 0  ]; then
+    help-replace-all-text
+    return 1
+  fi
+
+  if [[  $1 == "-h"  ]]; then
+    help-replace-all-text
+    return 0
+  fi
+
   grep -rlI "$1" | xargs sed -i "s/$1/$2/g"
 }
