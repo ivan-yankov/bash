268469370297fa6bebf7ebf7ce444bfdc7ce0be7
ivan.yankov <ivan.yankov@gmail.com>
2025-02-16 10:11:53 +0200

    Changes

--- text/find-text-ignore-case.sh
+++
@@ -1,7 +0,0 @@
-# dsc:Find text in the current directory recursively.
-# dsc:Text match is case insensitive.
-# arg:$1 text to search for
-function find-text-ignore-case {
-  is-defined $1 || return 1
-  grep -rIinH "$1"
-}
---
+++ text/ft.sh
@@ -0,0 +1,20 @@
+function help-ft {
+  echo "Find text in the current directory recursively."
+  echo
+  echo "Usage: ft [search-phrase]"
+}
+
+function ft {
+  if [[ $1 == "-h" ]]; then
+    help-ft
+    return 0
+  fi
+
+  local INITIAL_QUERY="${1:-}"
+
+  rg --color=always --line-number --no-heading --smart-case "$INITIAL_QUERY" 2>/dev/null | \
+  fzf --ansi \
+      --delimiter : \
+      --preview 'bat --style=numbers --color=always --highlight-line {2} {1} 2>/dev/null || cat {1}' \
+      --preview-window 'right:60%:+{2}'
+}
