aa932103a6cf9b504415ac9148fa977e7a9d3444
ivan.yankov <ivan.yankov@gmail.com>
2025-02-16 10:11:53 +0200

    Changes

--- media/convert-video.sh
+++ media/convert-video.sh
@@ -1,7 +1,15 @@
 function help-convert-video {
-  echo "Lossless video conversion."
+  echo "Video conversion."
+  echo "Supported formats mp4, webm."
+  echo "If video and audio codecs are compatible only container will be changed and content will not be re-encoded."
+  echo "Otherwise re-encoding will be performed as per provided parameters."
+  echo "Audio conversion is always lossless."
+  echo "Compression level does not affect quality, but conversion speed."
+  
   echo
-  echo "Usage: convert-video input-file output-file"
+  echo "Usage: convert-video input-file output-format video-quality compression-level"
+  echo "  video-quality: best | high | medium | low"
+  echo "  compression-level: high | medium | low"
 }
 
 function convert-video {
@@ -16,26 +24,74 @@ function convert-video {
   fi
 
   input="$1"
+  output_format="$2"
+  quality="#3"
+  compression="$4"
+
   filename=$(basename -- "$input")
-  extension="${filename##*.}"
   basename="${filename%.*}"
 
-  case "$extension" in
-    webm)
-      # WebM to MP4
-      output="${basename}.mp4"
-      echo "Converting WebM to MP4 (lossless video, high-quality audio)..."
-      ffmpeg -i "$input" -c:v libx264 -preset ultrafast -crf 0 -c:a aac -b:a 512k "$output"
+  cmp=placebo
+  case "$compression" in
+    high)
+      cmp=placebo      
       ;;
+    medium)
+      cmp=medium
+      ;;
+    low)
+      cmp=ultrafast
+      ;;
+  esac
+
+  crf=0
+  case "$quality" in
+    best)
+      crf=0
+      ;;
+    high)
+      crf=15
+      ;;
+    medium)
+      crf=25
+      ;;
+    low)
+      crf=50
+      ;;
+  esac
+
+  vcodec=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of csv=p=0 "$input")
+  acodec=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of csv=p=0 "$input")
+
+  compatible=false
+  if { [ "$vcodec" = "vp8" ] || [ "$vcodec" = "vp9" ] || [ "$vcodec" = "av1" ]; } && { [ "$acodec" = "vorbis" ] || [ "$acodec" = "opus" ]; }; then
+      compatible=true
+  fi
+
+  case "$output_format" in
     mp4)
-      # MP4 to WebM
+      output="${basename}.mp4"
+      if $compatible; then
+        echo "Codecs are compatible, conversion without re-encoding."
+        ffmpeg -i "$input" -c copy "$output"
+      else
+        echo "Codecs are not compatible, conversion with re-encoding."
+        ffmpeg -i "$input" -c:v libx264 -crf $crf -preset $cmp -c:a pcm_s16le "$output"
+      fi
+      ;;
+    webm)
       output="${basename}.webm"
-      echo "Converting MP4 to WebM (high quality VP9 + Opus)..."
-      ffmpeg -i "$input" -c:v libvpx-vp9 -crf 15 -b:v 0 -c:a libopus -b:a 512k "$output"
+      if $compatible; then
+        echo "Codecs are compatible, conversion without re-encoding."
+        ffmpeg -i "$input" -c copy "$output"
+      else
+        echo "Codecs are not compatible, conversion with re-encoding."
+        ffmpeg -i "$input" -c:v libvpx-vp9 -crf $crf -preset $cmp -c:a libopus -b:a 512k "$output"
+      fi
       ;;
     *)
       echo "Unsupported input file format: $extension"
-      echo "Only .mp4 and .webm are supported."
+      echo "Only mp4 and webm are supported."
       exit 1
       ;;
   esac
