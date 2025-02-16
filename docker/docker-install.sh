1f439c919be4bbb45eadf731ac191bc3bdbf5b4a
ivan.yankov <ivan.yankov@gmail.com>
2025-02-16 10:11:53 +0200

    Changes

--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -1,18 +1,30 @@
-# dsc:Install docker.
+function help-docker-install {
+  echo "Install Docker Engine."
+  echo
+  echo "Usage: docker-install os-name"
+  echo "  os-name: ubuntu | debian | ..."
+}
+
 function docker-install {
+  if [  $# -eq 0  ]; then
+    help-docker-install
+    return 1
+  fi
+
+  if [[  $1 == "-h"  ]]; then
+    help-docker-install
+    return 0
+  fi
+
+  local os_name=$1
+
   sudo apt-get update
-  sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-  sudo mkdir -m 0755 -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$os_name $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
 
   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -20,8 +32,5 @@ function docker-install {
   sudo groupadd docker
   sudo usermod -aG docker $USER
 
-  read -p "Docker installation completed. Reboot now (y/n): " rb
-  if [ $rb == "y" ]; then
-    reboot
-  fi
+  echo "Docker installation completed. Reboot the machine."
 }
