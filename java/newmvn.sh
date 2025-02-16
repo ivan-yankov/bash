8cc24e8cf4ce451761799c8dc5c198b437d1e100
ivan.yankov <ivan.yankov@gmail.com>
2025-02-16 10:11:53 +0200

    Changes

---
+++ java/newmvn.sh
@@ -0,0 +1,43 @@
+function help-newmvn {
+  echo "Create new maven project."
+  echo
+  echo "Usage: newmvn name main-class"
+}
+
+function newmvn {
+  if [  $# -eq 0  ]; then
+    help-newmvn
+    return 1
+  fi
+
+  if [[  $1 == "-h"  ]]; then
+    help-newmvn
+    return 0
+  fi
+
+  local name=$1
+  local main_class=$2
+  local package=com.example
+  local package_path=com/example
+  local run_file=$name/run
+  local version='1.0-SNAPSHOT'
+  local main_file=$name/src/main/java/$package_path/$name/$main_class.java
+
+  mvn archetype:generate -DgroupId=$package.$name -DartifactId=$name -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+  rm $name/src/main/java/$package_path/$name/*
+  rm $name/src/test/java/$package_path/$name/*
+
+  touch $main_file
+  echo "package $package;" > $main_file
+  echo "" >> $main_file
+  echo "public class $main_class {" >> $main_file
+  echo "  public static void main(String[] args) {" >> $main_file
+  echo "    System.out.println(\"Hello world!\");" >> $main_file
+  echo "  }" >> $main_file
+  echo "}" >> $main_file
+
+  touch $run_file
+  echo "mvn clean compile test package" > $run_file
+  echo "java -cp target/$name-$version.jar $package.$main_class" >> $run_file
+}
